e3c5c95bfffb2bf0cdd8cae53575a3d01deb101a
Adithyak-0926 <kanamarlapudi@e6x.io>
2024-12-13 12:41:34 +0530

    Refactored properly, removing all the unnecessary commas, spaces etc - 2

--- converter_api.py
+++ converter_api.py
@@ -18,7 +18,7 @@ def replace_struct_in_query(query):
     """
 
     # Define the regex pattern to match Struct(Struct(anything))
-    pattern = re.compile(r'Struct\s*\(\s*Struct\s*\(\s*([^\(\)]+)\s*\)\s*\)', re.IGNORECASE)
+    pattern = re.compile(r"Struct\s*\(\s*Struct\s*\(\s*([^\(\)]+)\s*\)\s*\)", re.IGNORECASE)
 
     # Function to perform the replacement
     def replace_match(match):
@@ -33,9 +33,7 @@ def replace_struct_in_query(query):
 
 @app.post("/convert-query")
 async def convert_query(
-        query: str = Form(...),
-        from_sql: str = Form(...),
-        to_sql: Optional[str] = Form("E6")
+    query: str = Form(...), from_sql: str = Form(...), to_sql: Optional[str] = Form("E6")
 ):
     try:
         # This is the main method will which help in transpiling to our e6data SQL dialects from other dialects
@@ -47,7 +45,9 @@ async def convert_query(
         converted_query = replace_struct_in_query(converted_query)
 
         converted_query_ast = parse_one(converted_query, read=to_sql)
-        double_quotes_added_query = quote_identifiers(converted_query_ast, dialect=to_sql).sql(dialect=to_sql)
+        double_quotes_added_query = quote_identifiers(converted_query_ast, dialect=to_sql).sql(
+            dialect=to_sql
+        )
 
         return {"converted_query": double_quotes_added_query}
     except Exception as e:
