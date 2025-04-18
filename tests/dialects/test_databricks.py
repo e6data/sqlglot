70ec053f3cba5843b5c990ae9f55290c6dc6ca7b
Adithyak-0926 <kanamarlapudi@e6x.io>
2025-04-18 16:39:17 +0530

    [CHORE]: Added test cases for GETDATE function from DBR.

--- tests/dialects/test_databricks.py
+++ tests/dialects/test_databricks.py
@@ -95,6 +95,16 @@ class TestDatabricks(Validator):
             },
         )
 
+        self.validate_all(
+            "SELECT GETDATE()",
+            write={
+                "databricks": "SELECT CURRENT_TIMESTAMP()",
+                "snowflake": "SELECT CURRENT_TIMESTAMP()",
+                "e6": "SELECT CURRENT_TIMESTAMP",
+                "trino": "SELECT CURRENT_TIMESTAMP"
+            }
+        )
+
         with self.assertRaises(ParseError):
             transpile(
                 "CREATE FUNCTION add_one(x INT) RETURNS INT LANGUAGE PYTHON AS $foo$def add_one(x):\n  return x+1$$",
