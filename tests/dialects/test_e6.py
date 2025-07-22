a60698132d5a0244b543e80b550d2b2b3c20429e
NiranjGaurav <niranj@e6x.io>
2025-07-22 18:41:11 +0530

    TRANSLATE to REPLACE function mapping as well as TYPEOF function mapping. But TYPEOF function doesnt support custom datatypes created at runtime

--- tests/dialects/test_e6.py
+++ tests/dialects/test_e6.py
@@ -43,6 +43,14 @@ class TestE6(Validator):
 
         )
 
+        self.validate_all(
+            "SELECT TYPEOF('hello')",
+        read={"databricks":"SELECT TYPEOF('hello');",
+              "spark":"SELECT TYPEOF('hello');",
+              "spark2":"SELECT TYPEOF('hello');",
+              "snowflake":"SELECT TYPEOF('hello');",}
+        )
+
         # Concat in dbr can accept many datatypes of args, but we map it to array_concat if type is of array. So we decided to put it as it is.
         self.validate_all(
             "SELECT CONCAT(TRANSFORM(ARRAY[1, 2], x -> x * 10), ARRAY[30, 40])",
