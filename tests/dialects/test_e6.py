e7a2ab2d13d7bfc208a82c8e1c27f3835b4cc0e6
Tanay Kulkarni <tanaykulkarni@Tanay-Kulkarni.local>
2025-12-22 10:51:07 +0530

    Remove parentheses around SELECT statements in UNION for E6 dialect

--- tests/dialects/test_e6.py
+++ tests/dialects/test_e6.py
@@ -2765,3 +2765,23 @@ class TestE6(Validator):
                 "databricks": "(\"time_col\" || ' hours')::INTERVAL",
             },
         )
+
+    def test_union_subquery_unwrap(self):
+        """Test that parentheses around SELECT statements in UNION are removed for E6."""
+
+        # Scenario 1: Simple UNION with CTE - parentheses around SELECTs should be removed
+        self.validate_all(
+            "WITH cte AS (SELECT * FROM test) SELECT col1 FROM test UNION ALL SELECT col2 FROM test",
+            read={
+                "databricks": "WITH cte AS (SELECT * FROM test) (SELECT col1 FROM test) UNION ALL (SELECT col2 FROM test)",
+            },
+        )
+
+        # Scenario 2: UNION with nested subquery in FROM clause
+        # Outer parentheses around SELECT removed, but subquery in FROM preserved
+        self.validate_all(
+            "WITH cte AS (SELECT * FROM test) SELECT col1 FROM (SELECT * FROM test) UNION ALL SELECT col1 FROM test",
+            read={
+                "databricks": "WITH cte AS (SELECT * FROM test) (SELECT col1 FROM (SELECT * FROM test)) UNION ALL (SELECT col1 FROM test)",
+            },
+        )
